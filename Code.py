37d35336d5858495e82e2e20cc4fdcc56338e9a4
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-05-16 12:57:18 +0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,6 +4,7 @@
 
 import random       # random library to take in random values
 
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
            'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
